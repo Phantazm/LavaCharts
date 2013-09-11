8073a8c66edc91072aee55b799813622d3aab2ed
Kevin Hill <kevinkhill@gmail.com>
2013-09-11 16:51:38 -0700

    moved drawer to top

--- public/js/drawer.js
+++ public/js/drawer.js
@@ -2,33 +2,30 @@ var drawer = {
     status : "closed",
     close : function()
     {
-	$('#slideout').animate({left:'-303px'}, {queue:false, duration: 250});
-	this.status = "closed";
+	$('#slideout').animate({top:'-352px'}, {queue:false, duration: 250});
+	drawer.status = "closed";
     },
     open : function()
     {
-	$('#slideout').animate({left:'0px'}, {queue:false, duration:400});
-	this.status = "open";
-    }
-};
-
-
-$(function(){
-    //Open & close drawer
-    $('#clickme').click(function() {
-        if(drawer.status == "closed") {
-            drawer.open();
-        } else {
-            drawer.close();
-        }
-    });
-
-    //Close drawer if click outside
-    $(document).click(function(event) {
-        if($(event.target).parents().index($('#slideout')) == -1) {
-            if($('#slideout').is(":visible")) {
+	$('#slideout').animate({top:'0px'}, {queue:false, duration:400});
+	drawer.status = "open";
+    },
+    bindTabClick : function() { //Open & close drawer
+            $('#clickme').click(function() {
+            if(drawer.status == "closed") {
+                drawer.open();
+            } else {
                 drawer.close();
             }
-        }
-    });
-});
+        });
+    },
+    bindOutsideClick : function() { //Close drawer if click outside
+        $(document).click(function(event) {
+            if($(event.target).parents().index($('#slideout')) == -1) {
+                if($('#slideout').is(":visible")) {
+                    drawer.close();
+                }
+            }
+        });
+    }
+};
